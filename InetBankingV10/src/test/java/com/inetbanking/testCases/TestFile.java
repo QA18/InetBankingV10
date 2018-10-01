b70e6b75aa54e4c4886694ff5ff4985fa2e54efd
QA18 <sr2002bd@yahoo.com>
2018-09-30 20:43:36 -0700

    Changes has done on TestFile.java

--- InetBankingV10/src/test/java/com/inetbanking/testCases/TestFile.java
+++ InetBankingV10/src/test/java/com/inetbanking/testCases/TestFile.java
@@ -1,1 +1,8 @@
 //This is Tesing file created to check pull activity
+//This is Tesing file created to check pull activity
+//This is Tesing file created to check pull activity
+//This is Tesing file created to check pull activity
+//This is Tesing file created to check pull activity
+//This is Tesing file created to check pull activity
+//This is Tesing file created to check pull activity
+
